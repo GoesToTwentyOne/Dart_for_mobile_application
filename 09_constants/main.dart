d6498e81387e3d2196528a79a866ddcd6c0967a1
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-16 16:18:55 +0600

    constant

---
+++ 09_constants/main.dart
@@ -0,0 +1,12 @@
+main(){
+  const i = 24;
+  const j = "Nihad";
+  const k = true;
+  print(i);
+  print(j);
+  print(k);
+
+  print(i.runtimeType);
+  print(j.runtimeType);
+  print(k.runtimeType);
+}
