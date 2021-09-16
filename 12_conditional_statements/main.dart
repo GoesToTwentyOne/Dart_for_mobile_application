523feca25d962288e13b4a8ee4eb275ec7aa7bef
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-16 21:26:15 +0600

    reiforce practice

---
+++ 12_conditional_statements/main.dart
@@ -0,0 +1,27 @@
+main(){
+  var x= 100;
+  if (x%2==0){
+    print("Even");
+
+  }else if (x%3==0){
+    print("Odd");
+  }else{
+    print("others");
+  }
+
+
+  var y=3;
+  switch(y){
+    case 1:
+      print("One");
+      break;
+    case 2:
+      print("Two");
+      break;
+    case 3:
+      print("Three");
+      break;
+    default:
+      print("Don't Match");
+  }
+}
