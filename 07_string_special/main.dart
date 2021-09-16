b3b509d1283e455edf791e11b45ee545d247e114
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-16 15:47:14 +0600

    rienforce string

---
+++ 07_string_special/main.dart
@@ -0,0 +1,32 @@
+main(){
+  var s1 ="This is Nihad Hossain";
+  var s2='This is Nihad Hossain';
+  var s3 ='It\'s Nihad practices';
+  var s4 = "It's Nihad Practices";
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  //raw string
+ var rs=r'It Nihad \n Practices';
+ print(rs);
+
+ //interpolation
+var name ="Nihad Hosain";
+var sprint= "My name is :$name";
+print(sprint);
+
+//multiline string
+var msting ="""
+My name is Nihad Hosain.
+I live in Uttara.
+""";
+  var msting2 ='''
+My name is Nihad Hosain.
+I live in Uttara.
+''';
+  print(msting);
+  print(msting2);
+
+
+}
