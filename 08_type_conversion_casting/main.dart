584ac484fbbd2601b4465437f7151a13c9c14c73
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-16 16:06:04 +0600

    type conversion

---
+++ 08_type_conversion_casting/main.dart
@@ -0,0 +1,23 @@
+main(){
+  //string to int
+  var intconv =int.parse('1');
+  if (intconv==1){
+    print(intconv);
+  }
+  // string to double
+  var doubleconv =double.parse('1.55');
+  if (doubleconv==1.55){
+    print(doubleconv);
+  }
+
+  //int to string
+  var intconv1=1.toString();
+  print(intconv1);
+
+
+  //double to string
+  var intconv2=1.55.toString();
+  print(intconv2);
+
+
+}
