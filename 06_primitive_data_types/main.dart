04b77274a4b9e3585c8765bfa58bf9a390f0ae73
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-16 15:27:44 +0600

    simple data types

---
+++ 06_primitive_data_types/main.dart
@@ -0,0 +1,18 @@
+main(){
+  //primitive_data_types
+  //string,bool,int,bool
+  // compile time
+  String  name ="Nihad Hossain";
+  var sname="Nihad Hossain";
+  print(sname);
+  print(name);
+  //dynamic
+  //rumtime
+
+  dynamic dvar ="Nihad Hossain";
+  print(dvar);
+  dynamic dvar2 =100;
+  print(dvar2);
+
+
+}
