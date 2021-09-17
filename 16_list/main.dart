fe9bb37599ecc4b2fd41e13a904f7eae9fd10675
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-09-17 19:27:35 +0600

    list

---
+++ 16_list/main.dart
@@ -0,0 +1,10 @@
+main(){
+  List x =[1,2,3,4,5];
+  print(x);
+  List <String> xx=["Nihad","Hossain","Tulip","Joy"];
+  for (var n in xx){
+    print(n);
+  }
+  var ycopy=x+[7,8,9];
+  print(ycopy);
+}
